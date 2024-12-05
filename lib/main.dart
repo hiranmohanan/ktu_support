f097b61e01968b7531a06393205824159a5fb7ae
hiranmohanan <hirancreative@gmail.com>
2024-12-05 15:38:34 +0530

    firebasre integration done

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Sizer(builder: (context, orientation, deviceType) {
       return const MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: LoginView(),
       );
     });
